c987dfbc2c7f51e33f9012b4c501be4f471366c5
Dustin Heeg <dustinheeg@gmail.com>
2023-11-11 08:51:10 -0600

    Update packages

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,17 +1,16 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     config = function()
-      local indent_blankline = require("indent_blankline")
+      local indent_blankline = require("ibl")
 
       indent_blankline.setup {
-        show_current_context = true,
-        buftype_exclude = { "terminal", "nofile" },
-        filetype_exclude = {
-          "help",
-          "packer",
-          "NvimTree",
-        },
+        scope = { enabled = true, show_start = false },
+        exclude = {
+          filetypes = { "help", "packer", "NvimTree" },
+          buftypes = { "terminal", "nofile" }
+        }
       }
     end
   }
