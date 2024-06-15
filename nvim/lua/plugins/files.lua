5d43b34e974bd75d1c73a7f8b87f956bed81e9dc
Dustin Heeg <dustinheeg@gmail.com>
2024-06-14 22:46:09 -0500

    Oil config

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -43,7 +43,12 @@
 -- }
 return {
   'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup {
+      view_options = {
+        show_hidden = true
+      }
+    }
+  end,
 }
