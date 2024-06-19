0a146ad8bd1c70f42f0dcdfe8ceadd0b38dbaa53
Dustin Heeg <dustinheeg@gmail.com>
2024-06-18 21:02:23 -0500

    Cleanup wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,77 +1,20 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.audible_bell = "Disabled"
+config.check_for_updates = false
 config.color_scheme = 'nord'
-config.font = wezterm.font('JetBrainsMono Nerd Font')
-config.harfbuzz_features = { "calt", "clig", "liga" }
-
--- config.keys = {
---   {
---     key = 's',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
---   },
---   {
---     key = 'v',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
---   },
---   {
---     key = 'h',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.ActivatePaneDirection("Left")
---   },
---   {
---     key = 'j',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.ActivatePaneDirection("Down")
---   },
---   {
---     key = 'k',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.ActivatePaneDirection("Up")
---   },
---   {
---     key = 'l',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.ActivatePaneDirection("Right")
---   },
---   {
---     key = 'q',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.CloseCurrentPane({ confirm = false })
---   },
---   {
---     key = '|',
---     mods = 'CTRL|SHIFT',
---     action = wezterm.action.TogglePaneZoomState
---   }
--- }
-
-config.use_fancy_tab_bar = false
 config.enable_scroll_bar = false
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
-config.window_decorations = "RESIZE"
+config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.hide_tab_bar_if_only_one_tab = true
+config.max_fps = 120
+config.mouse_wheel_scrolls_tabs = false
+config.warn_about_missing_glyphs = false
 config.window_close_confirmation = 'NeverPrompt'
-
-
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.font_size = 10
-end
 
 if wezterm.target_triple == 'x86_64-apple-darwin' then
   config.font_size = 13
@@ -82,5 +25,4 @@ if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
   config.font_size = 10
 end
 
--- and finally, return the configuration to wezterm
 return config
