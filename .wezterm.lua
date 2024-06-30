5ba5b2053bd75a27d28f547a050e9652db185e16
Dustin Heeg <dustinheeg@gmail.com>
2024-06-30 09:11:33 -0500

    Change font

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,7 +9,9 @@ config.audible_bell = "Disabled"
 config.check_for_updates = false
 config.color_scheme = 'nord'
 config.enable_scroll_bar = false
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+-- config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font('MonaspiceNe Nerd Font')
+config.harfbuzz_features = { "calt", "liga" }
 config.hide_tab_bar_if_only_one_tab = true
 config.max_fps = 120
 config.mouse_wheel_scrolls_tabs = false
@@ -17,7 +19,7 @@ config.warn_about_missing_glyphs = false
 config.window_close_confirmation = 'NeverPrompt'
 
 if wezterm.target_triple == 'aarch64-apple-darwin' then
-  config.font_size = 13
+  config.font_size = 14
   config.native_macos_fullscreen_mode = true
 end
 
