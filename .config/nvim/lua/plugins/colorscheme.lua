66c7fdb0030c4ab731033401d292e0f52827a24c
Dustin Heeg <dustinheeg@gmail.com>
2024-07-04 15:23:36 -0500

    Make diffing look nicer

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,22 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("nord").setup({ transparent = true })
+    require("nord").setup(
+      {
+        transparent = true,
+        on_highlights = function(highlights, colors)
+          local utils = require("nord.utils")
+          local alpha = 0.1
+          highlights.DiffAdd = { bg = utils.blend(colors.aurora.green, colors.polar_night.origin, alpha) }
+          highlights.DiffText = { bg = utils.blend(colors.aurora.yellow, colors.polar_night.origin, alpha + 0.10) }
+          highlights.DiffChange = { bg = utils.blend(colors.aurora.yellow, colors.polar_night.origin, alpha) }
+          highlights.DiffDelete = {
+            bg = utils.blend(colors.aurora.red, colors.polar_night.origin, alpha),
+            fg = utils.blend(colors.snow_storm.origin, colors.polar_night.origin, alpha)
+          }
+        end
+      }
+    )
     vim.cmd.colorscheme "nord"
   end
 }
