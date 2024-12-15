5099d1e9a1912980df35d55852b230cf16e5e8c0
Dustin Heeg <dustinheeg@gmail.com>
2024-12-15 12:52:36 -0600

    Switch from nvim-cmp to blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,62 +1,38 @@
 return {
-  "hrsh7th/nvim-cmp",
-  lazy = false,
-  priority = 100,
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- Buffer completions
-    "hrsh7th/cmp-path",   -- Path completions
-    {                     -- Snippet engine
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp"
+  'saghen/blink.cmp',
+  lazy = false, -- lazy loading handled internally
+  dependencies = 'rafamadriz/friendly-snippets',
+  -- use a release tag to download pre-built binaries
+  version = 'v0.*',
+  opts = {
+    keymap = { preset = 'default' },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'normal'
     },
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-    "hrsh7th/cmp-nvim-lsp", -- LSP completions
-    "hrsh7th/cmp-nvim-lsp-signature-help"
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    completion = {
+      menu = {
+        border = 'rounded',
+        draw = {
+          treesitter = {
+            'lsp'
+          },
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+        }
+      },
+      documentation = {
+        auto_show = true,
+        window = {
+          border = 'rounded'
+        }
+      },
+    },
   },
-  config = function()
-    local lspkind = require("lspkind")
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
 
-    cmp.setup({
-      sources = {
-        { name = "buffer" },
-        { name = "luasnip" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lsp_signature_help" },
-        { name = "nvim_lua" },
-        { name = "path" },
-      },
-      mapping = {
-        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-y>"] = cmp.mapping(
-          cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          }
-        ),
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-          show_labelDetails = true
-        }),
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      experimental = {
-        ghost_text = true
-      },
-    })
-  end
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -30,7 +30,7 @@ return {
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
 
     -- dartls is set up through flutter-tools.nvim
     require("flutter-tools").setup {
