fd72f0f656d1154d6ea4058f152a755c490dbc8f
Dustin Heeg <dustinheeg@gmail.com>
2025-11-23 19:21:56 -0600

    Treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+  lazy = false,
+  branch = "main",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
+  config = function()
+    local ts = require("nvim-treesitter")
+    local langs = {
       "c",
       "lua",
       "vim",
@@ -16,64 +19,96 @@ return {
       "html",
       "dart",
       "yaml",
-    },
-    sync_install = true,
-    auto_install = false,
-    highlight = { enable = true },
-    autopairs = { enable = true },
-    indent = { enable = true, disable = { "dart" } },
-    incremental_selection = { enable = true },
-    textobjects = {
-      select = {
-        enable = true,
+    }
+    ts.install(langs)
 
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
+    local ts_to = require("nvim-treesitter-textobjects")
+    ts_to.setup({ move = { set_jumps = true }, select = { lookahead = true } })
 
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/field region" },
-          ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/field region" },
+    -- select
+    local ts_to_s = require("nvim-treesitter-textobjects.select")
+    -- class
+    vim.keymap.set({ "n" }, "am", function()
+      ts_to_s.select_textobject("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "im", function()
+      ts_to_s.select_textobject("@class.inner", "textobjects")
+    end)
+    -- function
+    vim.keymap.set({ "n" }, "af", function()
+      ts_to_s.select_textobject("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "if", function()
+      ts_to_s.select_textobject("@function.inner", "textobjects")
+    end)
+    -- parameter
+    vim.keymap.set({ "n" }, "aa", function()
+      ts_to_s.select_textobject("@parameter.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "ia", function()
+      ts_to_s.select_textobject("@parameter.inner", "textobjects")
+    end)
+    -- block
+    vim.keymap.set({ "n" }, "ab", function()
+      ts_to_s.select_textobject("@block.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "ib", function()
+      ts_to_s.select_textobject("@block.inner", "textobjects")
+    end)
 
-          ["ab"] = { query = "@block.outer", desc = "Select outer part of a block region" }, -- overrides default text object block of parenthesis to parenthesis
-          ["ib"] = { query = "@block.inner", desc = "Select inner part of a block region" }, -- overrides default text object block of parenthesis to parenthesis
-
-          ["af"] = { query = "@function.outer", desc = "Select outer part of a function region" },
-          ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
-
-          ["am"] = { query = "@class.outer", desc = "Select outer part of a class region" },
-          ["im"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-        },
-        include_surrounding_whitespace = false,
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]f"] = { query = "@function.outer", desc = "Next method/function def start" },
-          ["]m"] = { query = "@class.outer", desc = "Next class start" },
-          ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
-          ["]b"] = { query = "@block.outer", desc = "Next block start" },
-        },
-        goto_next_end = {
-          ["]F"] = { query = "@function.outer", desc = "Next method/function def end" },
-          ["]M"] = { query = "@class.outer", desc = "Next class end" },
-          ["]A"] = { query = "@parameter.outer", desc = "Next parameter end" },
-          ["]B"] = { query = "@block.outer", desc = "Next block end" },
-        },
-        goto_previous_start = {
-          ["[f"] = { query = "@function.outer", desc = "Prev method/function def start" },
-          ["[m"] = { query = "@class.outer", desc = "Prev class start" },
-          ["[a"] = { query = "@parameter.outer", desc = "Prev parameter start" },
-          ["[b"] = { query = "@block.outer", desc = "Prev block start" },
-        },
-        goto_previous_end = {
-          ["[F"] = { query = "@function.outer", desc = "Prev method/function def end" },
-          ["[M"] = { query = "@class.outer", desc = "Prev class end" },
-          ["[A"] = { query = "@parameter.outer", desc = "Prev parameter end" },
-          ["[B"] = { query = "@block.outer", desc = "Prev block end" },
-        },
-      },
-    },
-  },
+    -- move
+    local ts_to_m = require("nvim-treesitter-textobjects.move")
+    -- class
+    vim.keymap.set({ "n" }, "]m", function()
+      ts_to_m.goto_next_start("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[m", function()
+      ts_to_m.goto_previous_start("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "]M", function()
+      ts_to_m.goto_next_end("@class.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[M", function()
+      ts_to_m.goto_previous_end("@class.outer", "textobjects")
+    end)
+    -- function
+    vim.keymap.set({ "n" }, "]f", function()
+      ts_to_m.goto_next_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[f", function()
+      ts_to_m.goto_previous_start("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "]F", function()
+      ts_to_m.goto_next_end("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[F", function()
+      ts_to_m.goto_previous_end("@function.outer", "textobjects")
+    end)
+    -- parameter
+    vim.keymap.set({ "n" }, "]p", function()
+      ts_to_m.goto_next_start("@parameter.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[p", function()
+      ts_to_m.goto_previous_start("@parameter.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "]P", function()
+      ts_to_m.goto_next_end("@parameter.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[P", function()
+      ts_to_m.goto_previous_end("@parameter.outer", "textobjects")
+    end)
+    -- block
+    vim.keymap.set({ "n" }, "]b", function()
+      ts_to_m.goto_next_start("@block.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[b", function()
+      ts_to_m.goto_previous_start("@block.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "]B", function()
+      ts_to_m.goto_next_end("@block.outer", "textobjects")
+    end)
+    vim.keymap.set({ "n" }, "[B", function()
+      ts_to_m.goto_previous_end("@block.outer", "textobjects")
+    end)
+  end,
 }
